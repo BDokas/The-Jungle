fda39e1de4085dfdb472434e312f55133c368883
Brian <brianadokas@gmail.com>
2021-10-17 22:00:52 -0400

    nice

--- upload.py
+++ upload.py
@@ -31,16 +31,11 @@ def Main():
                     print(f'Gallery: {gallery}, Name: {name}, IMG: {img}')
                     file_dir = os.path.join(subdir, img)
                     print(f'File: {file_dir}')
-                    post = requests.post(vault_url, headers={'access-token': 'zz+5^jas@(u42%^$ptyol!$47w*18*!2p*t=r!n9+mchtzdm%z'}, data={'name': name, 'gallery': gallery}, files={'file': open(file_dir, 'rb')})
+                    post = requests.post(vault_url, headers={}, data={'name': name, 'gallery': gallery}, files={'file': open(file_dir, 'rb')})
                     print(post.status_code)                    
-            #         break
-            #     break
-            # break
         i += 1
 
 
 if __name__ == "__main__":
-    # post = requests.post('http://15.222.77.108:8000/upload/', headers={'access-token': 'zz+5^jas@(u42%^$ptyol!$47w*18*!2p*t=r!n9+mchtzdm%z'}, data={'name': 'TEST_NAME1', 'gallery': 'TEST_GALLERY'}, files={'file': open('C:/Users/brian/Desktop/galleries\zion\Valley-Floor.jpg', 'rb')})
-    # print(post.text)
     Main()
 
